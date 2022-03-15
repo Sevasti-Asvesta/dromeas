59fc6e1af4bcfb2f00ae459c8817455a265c7fa2
Sevasti Asvesta <90143375+Sevasti-Asvesta@users.noreply.github.com>
2022-03-15 11:23:00 +0100

    changes

--- app.py
+++ app.py
@@ -172,7 +172,9 @@ def create_app(test_config=None):
     return app
 #maps code
 
-app= create_app()
+
 if __name__ == '__main__':
     #port = int(os.environ.get("PORT",5000))
     app.run(debug=True) #host='127.0.0.1',port=port,
+
+app= create_app()    
