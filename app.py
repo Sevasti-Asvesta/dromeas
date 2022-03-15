8054a77ad7dad053150e0c4c77ea840bfa1513b2
Sevasti Asvesta <90143375+Sevasti-Asvesta@users.noreply.github.com>
2022-03-15 09:37:04 +0100

    changes

--- app.py
+++ app.py
@@ -174,5 +174,5 @@ def create_app(test_config=None):
 
 app= create_app()
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT",5000))
-    app.run(host='127.0.0.1',port=port,debug=True)
+    #port = int(os.environ.get("PORT",5000))
+    app.run(debug=True) #host='127.0.0.1',port=port,
